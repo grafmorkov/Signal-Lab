3d9ed7d9ad9693e9c7f160c8412933ad6ce61287
grafmorkov <grafmorkov@gmail.com>
2025-10-25 14:09:35 +0300

    remade the Log type

--- Core/Utils/BinaryConverter.cs
+++ Core/Utils/BinaryConverter.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using SignalLab.Core.Logging;
 
 namespace SignalLab.Core.Utils;
 
@@ -6,11 +7,13 @@ public static class BinaryConverter
 {
     public static string ConvertToBinary(string text)
     {
+        ILogger logger = new FileLogger();
         if (string.IsNullOrEmpty(text))
             return string.Empty;
 
 
         byte[] bytes = Encoding.UTF8.GetBytes(text);
+        logger.Log($"Text: {text} has been converted to binary: {BitConverter.ToString(bytes)}.");
         return string.Concat(bytes.SelectMany(b => Convert.ToString(b, 2).PadLeft(8, '0')));
     }
 }
