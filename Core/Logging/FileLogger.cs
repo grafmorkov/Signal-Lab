1ac90442f15644a0de00a2511dcd8bd8bdf137b9
grafmorkov <grafmorkov@gmail.com>
2025-10-25 13:54:45 +0300

    Add File logger

---
+++ Core/Logging/FileLogger.cs
@@ -0,0 +1,17 @@
+using System.IO;
+
+namespace SignalLab.Core.Logging;
+
+public class FileLogger : ILogger
+{
+    private string _path = "log.txt";
+    public void Log(string message, LogType type = LogType.Info)
+    {
+        if (message != string.Empty)
+        {
+            var date = DateTime.Now;
+            string resultMessage = $"[{type.ToString().ToUpper()}][{date:HH:mm:ss}]: {message}";
+            File.AppendAllText(_path, resultMessage + Environment.NewLine);
+        }
+    }
+}
